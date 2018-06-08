cf6ef940918ea3cfc64df8d231860f3855c9733e
Ankit R Gadiya <git@argp.in>
2018-06-08 17:53:51 +0530

    Week4: Problem 2

--- week4/ps4a.py
+++ week4/ps4a.py
@@ -152,8 +152,10 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-
+    result = hand.copy()
+    for i in word:
+        result[i] -= 1
+    return result
 
 
 #
